ae4398dc52f501dc4eb4f04cfe441f3747cefe1d
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-11-06 00:21:26 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://tyftqbmhrxyxnropxbmf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+);
