a98abc4bc4c58d5186ebfb0a0fc70e3255e8a26c
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-11-06 00:28:44 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient.js";
+import { supabase } from "../lib/supabase.js";
 import {
   BarChart,
   Bar,
